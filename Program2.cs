db61166bedae7d08c42d6be6fe7f00c0357d95d6
p2ms <pamela.oliveira219@etec.sp.gov.br>
2022-03-09 22:04:11 -0300

    foi?

---
+++ Program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero=0;
+            int valor = 0; 
+
+
+            while(valor != 2)
+            {
+                if (numero == 0)
+                {
+                    numero = numero + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Errou!");
+                }
+                Console.WriteLine("jogo de adivinhação");
+                Console.WriteLine("digite um numero");
+                valor = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("acertou");
+            Console.ReadKey();
+
+        }
+    }
+}
